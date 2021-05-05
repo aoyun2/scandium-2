f66f7be37081a2dca33eb643411f3f3f7c5557c0
aaronthebaron77 <aaronthebaron77@outlook.com>
2021-05-05 13:24:02 -0500

    Update

--- commands/register.js
+++ commands/register.js
@@ -1,6 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+const redis = require("redis");
 
 module.exports.name = "register";
 
@@ -9,37 +10,41 @@ module.exports.run = async (bot, message, args) => {
     //await message.channel.send("Not implemented.");
     //return;
 
-    const data = fs.readFileSync('users.json');
-    const usersObj = JSON.parse(data);
-    //console.log(usersObj)
+    // const data = fs.readFileSync('users.json');
+    // const usersObj = JSON.parse(data);
+    // console.log(usersObj)
+    
+    const client = redis.createClient(process.env.REDIS_URL || "redis://:p4ddbbfa3213866833993a412cecf086db781eac1558af21fd0ef5f3d8ee2f335@ec2-184-72-229-210.compute-1.amazonaws.com:19029");
 
-    if (message.author.id in usersObj) {
-        const exampleEmbed2 = new Discord.MessageEmbed()
-            .setColor('#ff0000')
-            .setTitle("Already registered")
-            .setDescription(`You can login [here](https://scandium-2.herokuapp.com/) with the credentials:\n\n
-        UserID: \`${message.author.id}\`\n
-        Password: ||\`${usersObj[message.author.id]}\`||`);
-
-        if(message.guild) await message.reply("Check your DMs :mailbox_with_mail:")
-        return await message.author.send(exampleEmbed2);
-    }
+    client.get(message.author.id, (error, data) => {
+        if (data) {
+            const exampleEmbed2 = new Discord.MessageEmbed()
+                .setColor('#ff0000')
+                .setTitle("Already registered")
+                .setDescription(`You can login [here](https://scandium-2.herokuapp.com/) with the credentials:\n\n
+            UserID: \`${message.author.id}\`\n
+            Password: ||\`${data}\`||`);
+    
+            if(message.guild) await message.reply("Check your DMs :mailbox_with_mail:")
+            return await message.author.send(exampleEmbed2);
+        } else {
+            const exampleEmbed2 = new Discord.MessageEmbed()
+                .setColor('#00ff00')
+                .setTitle("Success!")
+                .setDescription(`You can login [here](https://scandium-2.herokuapp.com/) with the credentials:\n\n
+                UserID: \`${message.author.id}\`\n
+                Password: ||\`${pass}\`||`);
+            if(message.guild) await message.reply("Check your DMs :mailbox_with_mail:")
+            await message.author.send(exampleEmbed2);
+        }
+    });
 
     const pass = uuidv4();
 
-    const newObj = usersObj;
-    newObj[message.author.id] = pass;
-    console.log(newObj)
+    // const newObj = usersObj;
+    // newObj[message.author.id] = pass;
+    // console.log(newObj)
 
-    fs.writeFileSync('users.json', JSON.stringify(newObj));
-
-    const exampleEmbed2 = new Discord.MessageEmbed()
-        .setColor('#00ff00')
-        .setTitle("Success!")
-        .setDescription(`You can login [here](https://scandium-2.herokuapp.com/) with the credentials:\n\n
-        UserID: \`${message.author.id}\`\n
-        Password: ||\`${pass}\`||`);
-    if(message.guild) await message.reply("Check your DMs :mailbox_with_mail:")
-    await message.author.send(exampleEmbed2);
+    // fs.writeFileSync('users.json', JSON.stringify(newObj));
 }
 
