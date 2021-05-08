6b154854b70c9e91e4aa4f094ff0151f37439452
aaronthebaron77 <aaronthebaron77@outlook.com>
2021-05-08 16:03:49 -0500

    Favicon the 2nd

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use(express.static('./'))
 
 app.get("/", (request, response) => {
     response.render('home');
