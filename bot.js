554881983002a521d5190491cb65e0544ee3cca0
aaronthebaron77 <70720711+aaronthebaron77@users.noreply.github.com>
2021-10-13 21:20:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,13 +109,13 @@ bot.on("message", async (message) => {
 	if(message.guild) serverModule.broadcastMessage(await processMessage(message), { server: message.guild.id, channel: message.channel.id });
 
 	if (!message.content.includes(prefix) /*|| message.author.bot*/) { return; }
-	console.log(message);
+	//console.log(message);
 	var command = message.content.split(/\s+/g)[0];
-	var args = message.content.replace(`${command} `, '').split(';');
+	var args = message.content.replace(`${command} `, '').split('|');
 	if (args.includes(command)) args = [];
 
 	var cmd = await bot.commands.get(command.replace(prefix, ''));
-
+	console.log(command, args, cmd);
 	if (cmd) {
 		await cmd.run(bot, message, args);
 	}
