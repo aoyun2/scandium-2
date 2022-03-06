33a4e93d04d56ece575185700558ad76e0ecdf90
aaronthebaron77 <70720711+aaronthebaron77@users.noreply.github.com>
2022-03-06 15:22:31 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -471,7 +471,7 @@ module.exports.sendMessage = async (s, c, u, data, clientID) => {
 			avatarURL: user.user.avatarURL({dynamic: true}),
 			embeds: [embed]
 		});
-		m.suppressEmbeds(true);
+		await m.edit({ flags: ['SUPPRESS_EMBEDS'] });
 	} catch(e) {serverModule.error(e.message, clientID);};
 }
 
@@ -520,7 +520,7 @@ module.exports.replyToMessage = async (s, c, mid, u, data, clientID) => {
 			avatarURL: user.user.avatarURL({dynamic: true}),
 			embeds: [embed]
 		});
-		m.suppressEmbeds(true);
+		await m.edit({ flags: ['SUPPRESS_EMBEDS'] });
 	} catch (e) {serverModule.error(e.message, clientID);}
 }
 
