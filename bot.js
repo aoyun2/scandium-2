fa6236049c6783d6e080d8bda1b81e6781fea987
aaronthebaron77 <70720711+aaronthebaron77@users.noreply.github.com>
2022-08-19 13:55:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const prefix = botSettings.prefix;
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
-import { Blob } from 'buffer';
+import { Blob } from 'node:buffer';
 
 // inline reply code ------------------------------ //
 /*const { APIMessage, Webhook, Structures, Client } = require("discord.js");
