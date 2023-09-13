b7a29cc8e93fee643720752e8d6ad4c54df66f53
Aoyun <70720711+aoyun2@users.noreply.github.com>
2023-09-13 13:24:12 -0500

    Update talk.js

--- commands/talk.js
+++ commands/talk.js
@@ -14,7 +14,7 @@ async function fetchmessages(channel, limit = 20) {
 	}
 
 	const messages = await channel.messages.fetch(options);
-	sum_messages.unshift(...messages.array());
+	sum_messages.push(...messages.array().reverse());
 	last_id = messages.last().id;
 
 	if (messages.size != 100 || sum_messages >= limit) {
