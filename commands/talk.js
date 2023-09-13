92072c96a1dccb252879716838b4f21b4b5acae8
Aoyun <70720711+aoyun2@users.noreply.github.com>
2023-09-13 01:37:57 -0500

    Update talk.js

--- commands/talk.js
+++ commands/talk.js
@@ -3,12 +3,12 @@ const https = require('https');
 
 module.exports.name = "talk";
 
-async function fetchmessages(channel, limit = 50) {
+async function fetchmessages(channel, limit = 20) {
     const sum_messages = [];
     let last_id;
 
     while (true) {
-	const options = { limit: 50 };
+	const options = { limit: 20 };
 	if (last_id) {
 	    options.before = last_id;
 	}
