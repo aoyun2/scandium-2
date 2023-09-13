520aa30fc2aa3ddb73ea2268ae63e1d21402c09d
Aoyun <70720711+aoyun2@users.noreply.github.com>
2023-09-12 22:04:23 -0500

    Update talk.js

--- commands/talk.js
+++ commands/talk.js
@@ -8,7 +8,20 @@ module.exports.run = async (bot,message,args) => {
 	const { gpt } = require("gpti");
 	
 	gpt({
-	    prompt: "hello gpt, tell me what your version is?",
+	    prompt: `aoyun: too late now?
+Raindaggers: why was the hw so long bro
+seal stack: bc it is
+aoyun: @EpicLava
+EpicLava: maybe i'll join for like 20 minutes
+aoyun: bet
+triangularnotes22: Do we even need to do the hw correctly
+Raindaggers: no shot she's cehcking it unl;ess she got infinite time glitch
+triangularnotes22: I wish we could just submit random photos
+EpicLava: ms kopatic said every once in a while we will have an in-person homework check
+aoyun: 💀 bro is going through wormholes to grade the stats hw
+aoyun: ill join but im programming the bot rn
+Raindaggers: jaeho
+aoyun: what's my favorite color?`,
 	    model: "gpt-4",                         // code or model
 	    type: "json"                            // optional: "json" or "markdown"
 	}, (err, data) => {
