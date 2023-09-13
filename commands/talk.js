a5dd7280e6f90808c788e04f6328dfe505cb756c
Aoyun <70720711+aoyun2@users.noreply.github.com>
2023-09-13 18:03:55 -0500

    Update talk.js

--- commands/talk.js
+++ commands/talk.js
@@ -60,7 +60,7 @@ module.exports.run = async (bot,message,args) => {
 	
 	gpt({
 	    prompt: context,
-	    model: 6,                         // code or model
+	    model: "gpt4",                         // code or model
 	    type: "json"                            // optional: "json" or "markdown"
 	}, (err, data) => {
 	    if(err != null){
