30b4355c714bf19b2b0e3ec41021186c2d8fec00
Aoyun <70720711+aoyun2@users.noreply.github.com>
2023-09-15 19:03:51 -0500

    Update talk.js

--- commands/talk.js
+++ commands/talk.js
@@ -49,7 +49,7 @@ module.exports.run = async (bot,message,args) => {
 		if (m.id === message.id) continue;
 		//const member = (await m.guild).members.cache.find(member => member.id === m.author.id);
 		var c = (m.content.startsWith("<>talk")) ? m.content.replace("<>talk", '') : m.content;
-		context += `\n This is a past message, which was sent at ${m.createdAt.toDateString()} at ${m.createdAt.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}, please remember this information. However, today's date in the present is ${today.toDateString()}, and the time is ${today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}. Right now , in the present, Scandium is busy ${(activity === "eating" || activity === "doing homework" || activity === "sleeping") ? activity : " playing " + activity}.\n`;
+		context += `\n This is a past message, which was sent at ${m.createdAt.toDateString()} at ${m.createdAt.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}, please remember this information and use to to form your response: \n`;
 		context += (m.author.username + ": " + c + '\n');
 	}
 	//context += "\nThis is the current message to Scandium: \n";
