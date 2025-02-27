c44c54408497185dda6a27ebdda4227eb9fcacfc
Aoyun <70720711+aoyun2@users.noreply.github.com>
2025-02-27 11:17:59 -0600

    Update talk.js

--- commands/talk.js
+++ commands/talk.js
@@ -60,11 +60,13 @@ module.exports.run = async (bot,message,args) => {
 			//if (m.id === message.id) continue;
 			//const member = (await m.guild).members.cache.find(member => member.id === m.author.id);
 			var c = (m.cleanContent.startsWith("<>talk")) ? m.cleanContent.replace("<>talk", '') : m.cleanContent;
-			messages += `\nThe following text is a past message, which was sent at ${m.createdAt.toDateString()} at ${m.createdAt.toLocaleString('en-US', { hour: 'numeric', hour12: true })}:\n`;
+			context += `\nThe following text is a past message, which was sent at ${m.createdAt.toDateString()} at ${m.createdAt.toLocaleString('en-US', { hour: 'numeric', hour12: true })}:\n`;
 
 			var repliedTo;
 			if (m.reference) repliedTo = await m.channel.messages.fetch(m.reference.messageID);
-			messages += `${(m.author?m.author.username:m.bot.username + (repliedTo ? ", replying to " + (repliedTo.author?repliedTo.author.username:repliedTo.bot.username) : ""))}` + c + '\n';
+			var d = `${(m.author?m.author.username:m.bot.username + (repliedTo ? ", replying to " + (repliedTo.author?repliedTo.author.username:repliedTo.bot.username) : ""))}` + c + '\n';
+			messages += d;
+			context += d;
 		}
 		//context += "\nThis is the current message to Scandium: \n";
 		//context += (message.author.username + ": " + args.join(" ")) + "\n";
@@ -73,8 +75,8 @@ module.exports.run = async (bot,message,args) => {
 		const genAI = new GoogleGenerativeAI("AIzaSyAr67O7-mX9HHvfra6UhdmiCQEhJNzS9Ww");
 		const model = genAI.getGenerativeModel({model: "gemini-2.0-flash"});
 
-		const s = await model.generateContent("Can you generate a Google search query related to this conversation?\n" + messages);
-		//console.log(s.response.text());
+		const s = await model.generateContent("Can you generate a Google search query related to this conversation?\n" + messages.slice(-5));
+		console.log(s.response.text());
 		const queryResult = await search({
 			query: s.response.text(),
 			// OrganicResult is the default, however it is recommended to ALWAYS specify the result type
